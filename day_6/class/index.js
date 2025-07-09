6b981d0823b526decea230a57735b20d893801df
Kumari00Jyoti <jb134428@gmail.com>
2025-07-09 09:18:54 +0530

    day_1/class/index.html

---
+++ day_6/class/index.js
@@ -0,0 +1,78 @@
+       /* document.getElementById("heading").innerText="hi from script";
+        document.getElementById("heading").style.border="2px solid red"
+        document.getElementById("heading").style.width="500px"
+        document.getElementById("heading").style.padding="5px"
+        document.getElementById("heading").style.margin="auto"
+        document.getElementById("heading").style.textAlign="center"
+        consol.console.log("hello");
+        console.log(window)
+        console.log(document)*/
+
+        /*let a = 2;
+        let b = 4;
+        let c = 6;
+        console.log(a)*/
+
+        //function
+        /*function myfunction(){
+            let a = 10;
+            let b = 16;
+            //return a+b;
+            console.log(a+b)
+        }
+        myfunction()*/
+
+         /*myFun()
+       const myfunction = ()=>{
+            let a = 10;
+            let b = 16;
+            //return a+b;
+            console.log(a+b)
+            myfunction()
+        }
+        function myFun(){
+            a = 3;
+            b = 4;
+            console.log(a + b)
+            
+        }*/
+       /*console.log(a)
+       var a = 10;
+       a = 50;
+       console.log(a)*/
+
+
+   /*function myFun(x,y){
+         console.log(x + y)
+        }
+        let a= 30;
+        let b=40;
+        myFun(a,b)
+
+        function myFun1(x,y){
+            console.log(x,y)
+        }
+        let c = "jyoti";
+        let d ="bisht";
+        myFun1(c,d)*/
+
+        function myFun(){
+            let firstname = "jyoti";
+            let lastname = "bisht";
+            
+            function nestedFun(){
+                console.log(firstname,lastname)
+            }
+          nestedFun();    
+        }
+        myFun();
+
+        let name = "jyoti bisht"
+        console.log(name.length);
+       let length = name.length;
+       let firstchar = name[10];
+       let lastchar = name[length-1];
+       let splitchar = name.split( " ");
+
+        
+       console.log(name.)
---
+++ day_7/class/index.js
@@ -0,0 +1,163 @@
+/*let arr = [1,2,3,4,5,"jyoti",true,false,null,undefined]
+//console.log(arr[arr.length-1])
+
+arr.push("bisht")
+console.log(arr);
+
+arr.pop("bisht")
+
+arr[6] ="bisht"
+console.log(arr)
+console.log(arr.length)*/
+
+
+/*let arr = [78,"wild life",null,"@gmail",true,-91,undefined]
+console.log(arr)
+console.log(arr.length)
+
+arr.push('forest')
+console.log(arr)
+
+arr.pop(null)*/
+
+/*console.log(this)
+let obj ={
+    firstName:"jyoti",
+    lastName:"bisht",
+    name:"jyoti",
+    age:"20",
+    batch:"FS",
+   // subjects:["html","css","js"],
+   subjects:{
+    1:["chemistry","physics","maths"],
+    2:["ed","dsa","fs"],
+speciallization:{
+    1:"PPS",
+    2:"FS",
+    3:["SE","flat"]
+}
+   },
+
+    college:"RB",
+   /* myfunction:()=>{
+
+    }*/
+   /* myFunction:function(){
+       console.log(this.firstName,this.lastName)
+    }
+}
+obj.myFunction()
+obj.college ="RBIENT"*/
+//console.log(obj.myFunction())
+/*obj.myFunction()
+let output=obj.myFunction()
+console.log(output)
+console.log(obj.subjects.speciallization[3] [0])
+
+console.log(obj.subjects[2])*/
+
+/*let obj1={
+    eye:"brown",
+    ear:2,
+    fingers:10,
+    haircolor:"black",
+}
+//console.log(obj1)
+console.log(obj1.haircolor)
+console.log(obj1.eye)
+console.log(obj1.ear)
+console.log(obj1.fingers)
+
+for(let key in obj1){
+    console.log(key + ":" + obj1[key])
+}*/
+
+
+//conditins
+
+/*let a =20;
+let b = 31;
+if(a<b){
+    console.log("a is less than b")
+}
+else if(a==b){
+    console.log("a is equal to b")
+}else{
+    console.log("a is not less than b")
+}*/
+
+//loops
+//for,while,forof,forin
+
+//for loop
+/*let str = "john"
+for(let i =0;i<str.length;i++){
+    console.log(str[i])
+}*/
+
+//while
+/*let i = 1;
+while(i<10){
+console.log(i)
+i=i+1;
+}
+
+let str= "kamal"
+let n = str.length;
+for(let i=n;i>=0;i--){
+    console.log(str[i])
+}*/
+
+/*let arr=[1,2,3,4,5,6,7,8,9,10]
+let n =10
+for(let i=1;i<=n;i=i+0.5){
+    console.log(arr[i])
+}
+/*let str="anshika"
+for(let num of str){
+    console.log(str)
+}*/
+
+//forin 
+
+/*let obj ={
+    firstName:"jyoti",
+    lastName:"bisht",
+    name:"kirti",
+    age:20,
+    batch:"FS",
+   // subjects:["html","css","js"],
+   subjects:{
+    1:["chemistry","physics","maths"],
+    2:["ed","dsa","fs"],
+speciallization:{
+    1:"PPS",
+    2:"FS",
+    3:["SE","flat"]
+}
+   },
+
+    college:"RB",
+   /* myfunction:()=>{
+
+    }*/
+  /* myFunction:function(){
+       console.log(this.firstName,this.lastName)
+    }
+}
+/*for(let key in obj){
+    console.log(obj[key])
+    if(typeof(obj[key]=="string")){
+        console.log(obj[key])
+    }
+}*/
+
+/*let arr=[1,2,3,4,5,6,7,8,9]
+for(let i =0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        console.log('element ${arr[i] is even')
+    }
+}*/
+
+
+
