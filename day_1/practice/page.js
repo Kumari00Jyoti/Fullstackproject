2482e8b8b7cc05c6aa4d4999592fbbf694459ba1
Kumari00Jyoti <jb134428@gmail.com>
2025-07-12 21:54:43 +0530

    day_5/class/form.html

---
+++ day_1/practice/page.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("buttons")
+btn.addEventListener("click",function(){
+    btn.innerText="Booked"
+    alert("You Have Successfully Booked")
+})
+const btn1 = document.getElementById("buttons1")
+btn1.addEventListener("click",function(){
+    btn1.innerText="Details"
+    alert("Here Are the Details")
+})
---
+++ day_10/class/cart.js
@@ -0,0 +1,10 @@
+let data = [
+    {
+        "id":1,
+        "name":"Wireless Bluetooth Headphones",
+        "category":"Electronics",
+        "price":49.99,
+        "rating":4.5,
+        "image":
+    },
+]
